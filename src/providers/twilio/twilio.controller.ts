611c469bc69a1c4b2920c04d03e2124361d73ab1
Ruben <rubenl@balink.et>
2024-12-26 21:24:20 +0200

    feat: update Twilio voice response and change endpoint to POST method

--- src/providers/twilio/twilio.controller.ts
+++ src/providers/twilio/twilio.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Res } from '@nestjs/common';
+import { Controller, Post, Res } from '@nestjs/common';
 import { TwilioService } from './twilio.service';
 import { Response } from 'express';
 
@@ -10,7 +10,8 @@ export class TwilioController {
   async initiateCall(): Promise<void> {
     const userPhoneNumber = '+972587091495';
     const twilioPhoneNumber = '+972587091495';
-    const twimlUrl = 'http://localhost:3000/twilio/voice-response';
+    const twimlUrl =
+      'https://capitalmkt-automation-backend-09fa7b89d6dd.herokuapp.com/twilio/voice-response';
 
     await this.twilioService.initiateCall(
       userPhoneNumber,
@@ -19,7 +20,7 @@ export class TwilioController {
     );
   }
 
-  @Get('voice-response')
+  @Post('voice-response')
   async voiceResponse(@Res() res: Response): Promise<void> {
     res.type('text/xml');
     res.sendFile('response.xml', { root: './src/providers/twilio' });
